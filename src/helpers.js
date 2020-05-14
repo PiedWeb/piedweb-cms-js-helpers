a5b8e1087b6f152c0f937a109c709009befb7e7f
Robin <contact@robin-d.fr>
2020-05-14 17:27:19 +0200

    *

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,17 @@
 /**
+ * List of all functions
+ *
+ * - getBlockFromSky(attr)
+ * - formToSky(attr)
+ *
+ * - responsiveImage(string)    Relative to Liip filters
+ * - uncloakLinks(attr)
+ * - convertFormFromRot13(attr)
+ * - readableEmail(attr)
+ * - convertImageLinkToWebPLink()
+ */
+
+/**
  * Fetch (ajax) function permitting to get block via a POST request
  * (prevent from spam)
  *
@@ -135,24 +148,169 @@ export function responsiveImage(src) {
 }
 
 /**
- * Transform link to image produce with Liip to responsive Link
+ * Convert elements wich contain attribute (data-href) in normal link (a href)
+ * You can use a callback function to decrypt the link (eg: rot13ToText ;-))
  *
- * @param {string} attribute
- *
- * @example
- * <a href="/monImage/uneimage.png" data-rimg>
- * <a href="monimageopti.png">
+ * @param {string}  attribute
  */
-export function convertImgLinkToResponsiveImgLink(attribute = "data-rimg") {
-  var test = [];
-  if (typeof test.push === "function") {
-    // to avoid gooogle bot execution
+export async function uncloakLinks(attribute = "data-rot") {
+  var convertLink = function(element) {
+    // fix "bug" with img
+    if (element.getAttribute(attribute) === null) {
+      var element = element.closest("[" + attribute + "]");
+    }
+    if (element.getAttribute(attribute) === null) return;
+    var link = document.createElement("a");
+    var href = element.getAttribute(attribute);
+    element.removeAttribute(attribute);
+    for (var i = 0, n = element.attributes.length; i < n; i++) {
+      link.setAttribute(
+        element.attributes[i].nodeName,
+        element.attributes[i].nodeValue
+      );
+    }
+    link.innerHTML = element.innerHTML;
+    link.setAttribute(
+      "href",
+      responsiveImage(convertShortchutForLink(rot13ToText(href)))
+    );
+    element.parentNode.replaceChild(link, element);
+    return link;
+  };
+
+  var convertThemAll = function(attribute) {
     [].forEach.call(document.querySelectorAll("[" + attribute + "]"), function(
       element
     ) {
-      element.removeAttribute(attribute);
-      var href = element.getAttribute(href);
-      element.setAttribute("href", responsiveImage(href));
+      convertLink(element);
     });
+  };
+
+  var fireEventLinksBuilt = async function(element, event) {
+    await document.dispatchEvent(new Event("linksBuilt"));
+
+    var clickEvent = new Event(event.type);
+    element.dispatchEvent(clickEvent);
+  };
+
+  var convertLinkOnEvent = async function(event) {
+    // convert them all if it's an image (thanks this bug), permit to use gallery (baguetteBox)
+    if (event.target.tagName == "IMG") {
+      await convertThemAll(attribute);
+      var element = event.target;
+    } else {
+      var element = convertLink(event.target);
+    }
+    fireEventLinksBuilt(element, event);
+  };
+
+  [].forEach.call(document.querySelectorAll("[" + attribute + "]"), function(
+    element
+  ) {
+    element.addEventListener(
+      "touchstart",
+      function(e) {
+        convertLinkOnEvent(e);
+      },
+      { once: true }
+    );
+    element.addEventListener(
+      "click",
+      function(e) {
+        convertLinkOnFly(e);
+      },
+      { once: true }
+    );
+    element.addEventListener(
+      "mouseover",
+      function(e) {
+        convertInLinksRot13OnFly(e);
+      },
+      { once: true }
+    );
+  });
+}
+
+/**
+ * Convert action attr encoded in rot 13 to normal action with default attr `data-frot`
+ *
+ * @param {string}  attribute
+ */
+export function convertFormFromRot13(attribute = "data-frot") {
+  [].forEach.call(document.querySelectorAll("[" + attribute + "]"), function(
+    element
+  ) {
+    var action = element.getAttribute(attribute);
+    element.removeAttribute(attribute);
+    element.setAttribute(
+      "action",
+      convertShortchutForLink(rot13ToText(action))
+    );
+  });
+}
+
+export function convertShortchutForLink(str) {
+  if (str.charAt(0) == "-") {
+    return str.replace("-", "http://");
   }
+  if (str.charAt(0) == "_") {
+    return str.replace("_", "https://");
+  }
+  if (str.charAt(0) == "@") {
+    return str.replace("@", "mailto:");
+  }
+  return str;
+}
+
+/**
+ * readableEmail(selector) Transform an email encoded with rot13 in a readable mail (and add mailto:)
+ *
+ * @param {string}  text
+ */
+export function readableEmail(selector) {
+  document.querySelectorAll(selector).forEach(function(item) {
+    var mail = rot13ToText(item.textContent);
+    item.innerHTML = '<a href="mailto:' + mail + '">' + mail + "</a>";
+    if (selector.charAt(0) == ".") {
+      item.classList.remove(selector.substring(1));
+    }
+  });
+}
+
+/**
+ * Decode rot13
+ *
+ * @param {string}  str
+ */
+export function rot13ToText(str) {
+  return str.replace(/[a-zA-Z]/g, function(c) {
+    return String.fromCharCode(
+      (c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26
+    );
+  });
+}
+
+export function testWebPSupport() {
+  var elem = document.createElement("canvas");
+
+  if (elem.getContext && elem.getContext("2d")) {
+    return elem.toDataURL("image/webp").indexOf("data:image/webp") == 0;
+  }
+
+  return false;
+}
+
+/**
+ * Used in ThemeComponent
+ */
+export function convertImageLinkToWebPLink() {
+  var switchToWebP = function() {
+    [].forEach.call(document.querySelectorAll("a[dwl]"), function(element) {
+      var href = responsiveImage(element.getAttribute("dwl"));
+      element.setAttribute("href", href);
+      element.removeAttribute("dwl");
+    });
+  };
+
+  if (testWebPSupport()) switchToWebP();
 }
